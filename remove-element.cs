f331a4612810e6e88efe981879e4fb9c8d8034bd
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 16:01:58 -0500

    going foraward

--- remove-element.cs
+++ remove-element.cs
@@ -2,6 +2,20 @@
 
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        
+        for(int i = 0; i<nums.Length; i++){
+            if(nums[i] == val){
+                nums[i] = null;
+                for(int j = 1; j< nums.Length; j++){
+                    if(nums[i+j] == val) nums[i+j] = null;
+                    else{
+                        int change = nums[j];
+                        nums[i] = change;
+                        nums[j] = null;
+
+
+                    }
+                }
+            }
+        }   
     }
 }
