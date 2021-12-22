04726fa9cbef7e6d5735577928bbae948d00e762
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:28:08 -0500

    longest-common-prefix added

---
+++ longest-common-prefix.cs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        for(int i = 0; i < ; i++){
+            
+        }
+    }
+}
