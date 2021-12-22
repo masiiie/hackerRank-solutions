c04bd8784f9f9df714ef49a495681fe686f30917
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 18:57:53 -0500

    going forward

--- add-two-numbers.cs
+++ add-two-numbers.cs
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/add-two-numbers/
-    
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,6 +13,19 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        
+        int number1 = 0;
+        int number2 = 0;
+
+        for(int i = 0; i < 100; i++){
+            if(l1 == null && l2 == null) break;
+            if(l1 != null ){
+                number1 += l1.val*Math.Pow(10,i);
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                number2 += l2.val*Math.Pow(10,i);
+                l2 = l2.next;
+            }
+        }
     }
 }
