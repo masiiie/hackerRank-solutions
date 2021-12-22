b56f548b0009fc52452c0c06a6aa7820ce3842a5
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 16:20:54 -0500

    wrong answer

--- remove-element.cs
+++ remove-element.cs
@@ -2,18 +2,23 @@
 
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
+        int k = 0;
         for(int i = 0; i<nums.Length; i++){
             int lastnull = -1;
             if(nums[i] == val){
                 lastnull = i;
-                nums[i] = null;
+                nums[i] = -1;
             }
-            else if(lastnull!=-1){
-                int change = nums[i];
-                nums[lastnull] = change;
-                nums[i] = null;
-                lastnull+=1;
+            else{
+                k += 1;
+                if(lastnull!=-1){
+                    int change = nums[i];
+                    nums[lastnull] = change;
+                    nums[i] = -1;
+                    lastnull+=1;
+                }
             }
         }   
+        return k;
     }
 }
