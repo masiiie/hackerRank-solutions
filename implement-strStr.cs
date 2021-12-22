f0b9af49a92d52e5a920d2d525739d0e52f09826
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 15:33:47 -0500

    going forward

--- implement-strStr.cs
+++ implement-strStr.cs
@@ -3,6 +3,14 @@
 
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        
+        if(needle.Length == 0) return 0;
+        int index = -1;
+        for(int i = 0; i < haystack.Length - needle.Length; i++){
+            for(int j = 0; j < needle.Length; j++){
+                if(haystack[i+j]!=needle[j]) break;
+            }
+            if(j==needle.Length) break;
+        }
+        return index;
     }
 }
