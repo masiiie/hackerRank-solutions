b265e6ec690b760d2fb0bb76364902d050418719
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:16:36 -0500

    going forward

--- roman-to-integer.cs
+++ roman-to-integer.cs
@@ -1,5 +1,21 @@
+// https://leetcode.com/problems/roman-to-integer/
+
 public class Solution {
     public int RomanToInt(string s) {
-        
+        Dictionary<char,int> values = new Dictionary<char, int>{
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+
+        int sol = 0;
+
+        for(int i=0; i<s.Length; i++){
+
+        }
     }
 }
