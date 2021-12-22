325918390eeef370cf0b562a81eae9559f19af70
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:38:17 -0500

    almost done!

--- longest-common-prefix.cs
+++ longest-common-prefix.cs
@@ -2,17 +2,16 @@
 
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
+        if(strs.Length == 1) return strs[0];
         string longest = "";
         for(int i = 0; i < 200; i++){
-            for(int j = 0; j < strs.Length; j++){
-                if(i < strs[j].Length)
-                {
-
-                }
-                else{
-                    return longest;
-                }
+            char actual;
+            if(i < strs[0].Length) actual = strs[0][i];
+            else return longest;
+            for(int j = 1; j < strs.Length; j++){
+                if(i >= strs[j].Length or strs[j][i] != actual) return longest;
             }
+            longest += actual;
         }
 
         return longest;
