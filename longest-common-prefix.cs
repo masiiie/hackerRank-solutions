6ba760fb19447628058289d2fa164b06cb8c1f2d
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:31:43 -0500

    going forward

--- longest-common-prefix.cs
+++ longest-common-prefix.cs
@@ -2,8 +2,19 @@
 
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        for(int i = 0; i < ; i++){
-            
+        string longest = "";
+        for(int i = 0; i < 200; i++){
+            for(int j = 0; j < strs.Length; j++){
+                if(i < strs[j].Length)
+                {
+
+                }
+                else{
+                    return longest;
+                }
+            }
         }
+
+        return longest;
     }
 }
