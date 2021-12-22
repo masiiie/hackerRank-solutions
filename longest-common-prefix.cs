d50cadbc46f3f6b2bf7fb73ccb0d85521f9c5499
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:39:38 -0500

    success!!

--- longest-common-prefix.cs
+++ longest-common-prefix.cs
@@ -9,7 +9,7 @@ public class Solution {
             if(i < strs[0].Length) actual = strs[0][i];
             else return longest;
             for(int j = 1; j < strs.Length; j++){
-                if(i >= strs[j].Length or strs[j][i] != actual) return longest;
+                if(i >= strs[j].Length || strs[j][i] != actual) return longest;
             }
             longest += actual;
         }
