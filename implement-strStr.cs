30438c9da8eb813f7565b99e0642754afcc40504
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 15:42:44 -0500

    sucess!! Runtime: 328 ms, faster than 70.42% of C# online submissions for Implement strStr(). Memory Usage: 37.3 MB, less than 43.70% of C# online submissions for Implement strStr().

--- implement-strStr.cs
+++ implement-strStr.cs
@@ -5,7 +5,7 @@ public class Solution {
     public int StrStr(string haystack, string needle) {
         if(needle.Length == 0) return 0;
         int index = -1;
-        for(int i = 0; i < haystack.Length - needle.Length; i++){
+        for(int i = 0; i < haystack.Length - needle.Length + 1; i++){
             for(int j = 0; j < needle.Length; j++){
                 if(haystack[i+j]==needle[j]) index = i;
                 else{
