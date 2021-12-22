6523c296d61af2073906265da0fa902ccb965b51
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:56:48 -0500

    going forward

--- merge-two-sorted-list.cs
+++ merge-two-sorted-list.cs
@@ -15,8 +15,16 @@ public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
         ListNode actual = new ListNode(0);
 
-        while(list1.next != null or list2.next != null){
-            if(list1.val < list2.val){
+        while(list1 != null || list2 != null){
+            if(list1 == null){
+                actual.val = list2.val;
+                actual.next = list2.next.Copy()
+            } 
+            else if(list2 == null){
+                actual.val = list1.val;
+                actual.next = list1.next.Copy();
+            }
+            else if(list1.val < list2.val){
                 actual.val = list1.val;
                 list1 = list1.next;
             }
@@ -24,7 +32,10 @@ public class Solution {
                 actual.val = list2.val;
                 list2 = list2.next;
             }
-        
-        }   
+            actual.next = new ListNode(0);
+            actual = actual.next;
+        }  
+
+        actual.next = null; 
     }
 }
