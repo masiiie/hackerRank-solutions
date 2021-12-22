12c3fdd9543faac7b006bdb85fbe3585a2b98e98
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:04:53 -0500

    done! I think...

--- two-su.cs
+++ two-su.cs
@@ -3,6 +3,12 @@
 
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        
+
+        for(int i = 0; i < nums.Length; i++){
+            if(i >= target) continue;
+            for(int j = i + 1; j < nums.Length; j++){
+                if(nums[i] + nums[j] == target) return new int[]{i,j};
+            }
+        }
     }
 }
