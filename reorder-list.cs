1a9f88fc06b9483c2e988bf82b4862e046aaa290
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 16:31:52 -0500

    reorder-list added

---
+++ reorder-list.cs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/reorder-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public void ReorderList(ListNode head) {
+        
+    }
+}
