7438c42115f6faee2a61143299cba7a481d22fd4
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-22 14:50:44 -0500

    going forward

--- merge-two-sorted-list.cs
+++ merge-two-sorted-list.cs
@@ -13,6 +13,18 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode actual = new ListNode(0);
+
+        while(list1.next != null or list2.next != null){
+            if(list1.val < list2.val){
+                actual.val = list1.val;
+                list1 = list1.next;
+            }
+            else if(list2.val <= list1.val){
+                actual.val = list2.val;
+                list2 = list2.next;
+            }
         
+        }   
     }
 }
