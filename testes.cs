dbac8504c05ebfec57b02a0ad9280655216503ca
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-28 15:58:16 -0500

    wrong answer

---
+++ testes.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");  
+    }
+}
--- valid-parentheses.cs
+++ valid-parentheses.cs
@@ -1,5 +1,33 @@
 public class Solution {
     public bool IsValid(string s) {
-        
+        int t1 = 0;
+        int t2 = 0;
+        int t3 = 0;
+
+        for(int i=0; i<s.Length; i++){
+            switch(s[i]) 
+            {
+                case '(':
+                    t1+=1;
+                    break;
+                case ')':
+                    t1-=1;
+                    break;
+                case '{':
+                    t2+=1;
+                    break;
+                case '}':
+                    t2-=1;
+                    break;
+                case '[':
+                    t3+=1;
+                    break;
+                default:
+                    t3-=1;
+                    break;
+            }
+        }
+
+        return t1==0 && t2==0 && t3 == 0;
     }
 }
