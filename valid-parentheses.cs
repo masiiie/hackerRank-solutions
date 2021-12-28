f7d3ce553b1ec930fdef4775b82092614ddad484
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-28 16:46:50 -0500

    Runtime: 76 ms, faster than 82.12% of C# online submissions for Valid Parentheses. Memory Usage: 37.7 MB, less than 13.41% of C# online submissions for Valid Parentheses.

--- valid-parentheses.cs
+++ valid-parentheses.cs
@@ -1,34 +1,47 @@
 // https://leetcode.com/problems/valid-parentheses/submissions/
 
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        sol.IsValid(")(");
+    }
+}
+
 public class Solution {
     public bool IsValid(string s) {
-        int t1 = 0;
-        int t2 = 0;
-        int t3 = 0;
+        Stack<char> stack = new Stack<char>();
 
         for(int i=0; i<s.Length; i++){
             switch(s[i]) 
             {
                 case '(':
-                    t1+=1;
+                    stack.Push(s[i]);
                     break;
                 case ')':
-                    t1-=1;
+                    if(stack.Count > 0 && stack.Peek() == '(') stack.Pop();
+                    else return false;
                     break;
                 case '{':
-                    t2+=1;
+                    stack.Push(s[i]);
                     break;
                 case '}':
-                    t2-=1;
+                    if(stack.Count > 0 && stack.Peek() == '{') stack.Pop();
+                    else return false;
                     break;
                 case '[':
-                    t3+=1;
+                    stack.Push(s[i]);
                     break;
                 default:
-                    t3-=1;
+                    if(stack.Count > 0 && stack.Peek() == '[') stack.Pop();
+                    else return false;
                     break;
             }
         }
-        return t1==0 && t2==0 && t3 == 0;
+        return stack.Count == 0;
     }
 }
