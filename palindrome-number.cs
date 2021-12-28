8a10d93d43da9b139c7be9859a80179cd521ad3f
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-28 15:27:20 -0500

    done!

--- palindrome-number.cs
+++ palindrome-number.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        
+        string ToString = x.ToString();
+        return IsPalindrome(ToString);
+    }
+
+    bool IsPalindrome(string x){
+        if(x.Length == 1) return true;
+        if(x.Length == 0) return true;
+        if(x[0] != x[x.Length - 1]) return false;
+        return IsPalindrome(x.Substring(1, x.Length-2));
     }
 }
