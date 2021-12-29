3dd3b257fca594a6121409c11a90e8841896c72f
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-28 22:21:55 -0500

    going forward

--- count-and-say.cs
+++ count-and-say.cs
@@ -1,7 +1,26 @@
 // https://leetcode.com/problems/count-and-say/
 
+using System.Collections.Generic;
+using System;
+
+public 
+
 public class Solution {
     public string CountAndSay(int n) {
         
+    }
+
+    public string say(string chain){
+        Dictionary<char,int> dict = new Dictionary<char, int>();
+
+        for(int i=0; i<chain.Length; i++){
+            dict[chain[i]]++;
+        }
+        
+        string sol = "";
+        foreach(char key in dict.Keys){
+            sol+=dict[key].ToString()+key;
+        }
+        return sol;
     }
 }
