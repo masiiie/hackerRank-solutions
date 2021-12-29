b11edd3512f5d46391c85adda30338717a5dc122
masiiie <villalbamasiel@gmail.com>
2021-12-29 14:54:25 +0100

    going forward

--- add-two-numbers.cs
+++ add-two-numbers.cs
@@ -2,46 +2,88 @@
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
+ * 
  */
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        ListNode l1 = new ListNode(7, 
+            new ListNode(8,
+                new ListNode(7)));
+        ListNode l3 = new ListNode(9);
+        ListNode l2 = new ListNode(1, 
+            new ListNode(9,
+                new ListNode(9,
+                    new ListNode(9,
+                        new ListNode(9,
+                            new ListNode(9,
+                                new ListNode(9,
+                                    new ListNode(9,
+                                        new ListNode(9,
+                                            new ListNode(9))))
+                                ))))));
+
+        // 787 + 913 = 1700
+        ListNode sum = sol.AddTwoNumbers(l3,l2);
+        while(sum!=null){
+            Console.Write(sum.val);
+            sum=sum.next;
+        }
+    }
+}
+
+public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+         this.next = next;
+      }
+  }
+
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        long number1 = 0;
-        long number2 = 0;
+        ListNode solution = new ListNode(0);
+        ListNode solutionIndex = solution;
+        int carry = 0;
+        int s = 0;
 
-        for(int i = 0; i < 100; i++){
-            if(l1 == null && l2 == null) break;
-            if(l1 != null ){
-                number1 += l1.val*Convert.ToInt32(Math.Pow(10,i));
-                l1 = l1.next;
-            }
-            if(l2 != null){
-                number2 += l2.val*Convert.ToInt32(Math.Pow(10,i));
+        while(l1 != null | l2 != null){
+            if(l1 == null ){
+                s = l2.val + carry;
                 l2 = l2.next;
             }
-        }
-
-        long suma = number1 + number2;
-        string sumaString = suma.ToString();
-        ListNode realActual = new ListNode(0);        
-        ListNode actual = realActual;
-
-        for(int i= sumaString.Length - 1; i > -1; i--){
-            int.TryParse(sumaString.Substring(i,1), out actual.val);
-
-            if(i>0){
-                actual.next = new ListNode(0);
-                actual = actual.next;
+            else if(l2 == null){
+                s = l1.val + carry;
+                l1 = l1.next;
             }
+            else{
+                s = l1.val + l2.val + carry;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            if(s>9){
+                carry = 1;
+                s = s - 10;
+            }
+            else carry = 0; 
+
+            solutionIndex.val = s;
+            solutionIndex.next = new ListNode(0);
+            solutionIndex = solutionIndex.next;
         }
 
-        return realActual;
+      
+
+        if(carry==0) solutionIndex.next = null;
+        else solutionIndex.val = carry;
+
+        return solution;
     }
 }
