d85b7536a6336b03207e4b08aec2ebb856d3244c
masiiie <villalbamasiel@gmail.com>
2021-12-30 14:49:49 +0100

    length-of-last-word added

---
+++ length-of-last-word.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");  
+    }
+}
+
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        string[] chars = s.Split(' ');
+        if(chars[chars.Length-1]==' ') return chars[]
+    }
+}
