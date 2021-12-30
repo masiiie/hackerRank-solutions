af40711a4b5d29d3184bcc45c07c7ffb26f1adf8
masiiie <villalbamasiel@gmail.com>
2021-12-30 15:06:20 +0100

    Runtime: 103 ms, faster than 10.06% of C# online submissions for Length of Last Word. Memory Usage: 34.6 MB, less than 96.09% of C# online submissions for Length of Last Word.

--- length-of-last-word.cs
+++ length-of-last-word.cs
@@ -6,17 +6,23 @@ public class Program
     public static void Main(string[] args)
     {
         Solution sol = new Solution();
-        Console.WriteLine(sol.LengthOfLastWord("Hola te amo                         ejeje"));
+        Console.WriteLine(sol.LengthOfLastWord("luffy is still joyboy"));
     }
 }
 
 public class Solution {
     public int LengthOfLastWord(string s) {
+        int lastlen = 0;
         int len = 0;
         for(int i=0;i<s.Length;i++){
             if(s[i]!=' ') len++;
-            else len = 0;
+            else{
+                if(len!=0) lastlen = len;
+                len = 0;
+            }
         }
+
+        if(len==0 && lastlen!=0) return lastlen;
         return len;
     }
 }
