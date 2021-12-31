6bb87c281ae99652dbac596010e337a3236cd1d2
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-31 11:31:48 -0500

    done! I think...

--- median-two-sorted-array.cs
+++ median-two-sorted-array.cs
@@ -40,12 +40,17 @@ public class Program
 
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[,] candidatos = new int[2,3];
+        int[,] options = new int[2,3]{
+                Array.Fill(new int[3], Math.Pow(-10,7)),
+                Array.Fill(new int[3], Math.Pow(-10,7))
+            };
         int totalLen = nums1.Length + nums2.Length;
         int umbral = totalLen%2!=0 ? (totalLen-1)/2 : totalLen/2;
         
-        Func<int,int,bool> descartado = (int menores,int mayores) => {
-            return menores>umbral || mayores>umbral || menores<0 || mayores<0;
+        Func<int,int,int> isDeprecated = (int menores,int mayores) => {
+            if(menores>umbral) return 1;
+            if(mayores>umbral) return 2;
+            return 0;
         };   
 
         Func<int,int,bool> isMedian = (int menores,int mayores) => {
@@ -60,9 +65,53 @@ public class Solution {
         while(true){
             int posmedian1 = (i11 + i12)/2;
             int posmedian2 = (i21 + i22)/2;
-            if(descartado(candidatos[0,1], candidatos[0,2])){
-                               
+            
+            if(options[0,0]==Math.Pow(-10,7)){
+                options[0,0] = nums1[posmedian1];
+                options[0,1] = posmedian1;
+                options[0,2] = nums1.Length - posmedian1 - 1;               
+            }
+            if(options[1,0]==Math.Pow(-10,7)){
+                options[1,0]=nums2[posmedian2];
+                options[1,1]=posmedian2;
+                options[1,2]=nums2.Length - posmedian2 - 1;
+            }
+
+
+            if(options[0,0]<=options[1,0]){
+                options[1,1]+=1+options[0,1];
+                options[0,2]+=1+options[1,2];
+            }
+            else{
+                options[0,1]+=1+options[1,1];
+                options[1,2]+=1+options[0,2];
+            }
+
+            bool cond1 = isMedian(options[0,1], options[0,2]);
+            bool cond2 = isMedian(options[1,1], options[1,2]);
+
+            if(totalLen%2!=0 && cond1) return options[0,0];
+            else if(totalLen%2!=0 && cond2) return options[1,0];
+            else if(totalLen%2==0 && cond1 && cond2) return (options[0,0], options[1,0])/2;
+
+            int dep1 = isDeprecated(options[0,1], options[0,2]);
+            int dep2 = isDeprecated(options[1,1], options[1,2]);
+
+            if(dep1>0){
+                if(options[0,0]<=options[1,0]) options[1,1]-=(1+options[0,1]);
+                else options[1,2]-=(1+options[0,2]);
+                if(dep1==1) i12 = posmedian1-1;
+                else i11=posmedian1+1;
+                options[0,0] = Math.Pow(-10,7);
+            }
+            else if(dep2>0){
+                if(options[1,0]<=options[0,0]) options[0,1]-=(1+options[1,1]);
+                else options[0,2] -= (1+options[1,2]);
+                if(dep2==1) i22 = posmedian2-1;
+                else i21=posmedian2+1;
+                options[1,0] = Math.Pow(-10,7);
             }
         }
+        return Math.Pow(-10, 7);
     }
 }
