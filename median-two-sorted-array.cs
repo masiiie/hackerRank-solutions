4caeaa98d510871c2f572694f87a5e23cc3c3756
Masiel Villalba <villalbamasiel@gmail.com>
2021-12-31 10:42:01 -0500

    changing all

--- median-two-sorted-array.cs
+++ median-two-sorted-array.cs
@@ -1,5 +1,6 @@
 using System;
-					
+using System.Collections.Generic;
+
 public class Program
 {
 	public static void Main()
@@ -39,56 +40,29 @@ public class Program
 
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.Length;
-        int m = nums2.Length;
-        return FindMedianSortedArrays(nums1, nums2, 0, n-1, 0, m-1, new int[n+m], -1);
-    }
+        int[,] candidatos = new int[2,3];
+        int totalLen = nums1.Length + nums2.Length;
+        int umbral = totalLen%2!=0 ? (totalLen-1)/2 : totalLen/2;
+        
+        Func<int,int,bool> descartado = (int menores,int mayores) => {
+            return menores>umbral || mayores>umbral || menores<0 || mayores<0;
+        };   
 
-    public double FindMedianSortedArrays(int[] nums1, int[] nums2, int i11, int i12, int i21, int i22, int[] auxiliar, int lastFilled){
-        int auxLen = auxiliar.Length;
+        Func<int,int,bool> isMedian = (int menores,int mayores) => {
+            return (menores+mayores+1)==totalLen && Math.Abs(mayores-menores)<=1;
+        }; 
 
-        if(lastFilled>=(auxLen-1)/2){
-            if(auxLen%2==0) return (auxiliar[auxLen/2] + auxiliar[(auxLen/2)-1])/2;
-            else return auxiliar[(auxLen-1)/2];
-        }
 
-        Console.WriteLine("i11={0} i12={1}", new object[2]{i11,i12});
-        Console.WriteLine("i21={0} i22={1}", new object[2]{i21,i22});
-        for(int i=0;i<auxLen;i++){
-            Console.Write("{0} ", auxiliar[i]);
-        }
-        Console.WriteLine("lastFilled={0}",lastFilled);
-        Console.WriteLine();
-        //Console.ReadLine();
+        int i11 = 0;    int i12=nums1.Length-1;
+        int i21 = 0;    int i22=nums2.Length-1;
 
-        int pos1 = i11 + i12;
-        int pos2 = i21 + i22;
 
-        int median1pos = pos1%2==0 ? pos1/2 : (pos1-1)/2;
-        int median2pos = pos2%2==0 ? pos2/2 : (pos2-1)/2; 
-
-        int median1 = nums1[median1pos];
-        int median2 = nums2[median2pos];
-
-        if(median1<=median2){
-            while(median1> nums2[i21]){
-                lastFilled++;
-                auxiliar[lastFilled] = nums2[i21];
-                i21++;
+        while(true){
+            int posmedian1 = (i11 + i12)/2;
+            int posmedian2 = (i21 + i22)/2;
+            if(descartado(candidatos[0,1], candidatos[0,2])){
+                               
             }
-
-            for(int i=i11; i<median1pos;i++){
-                lastFilled++;
-                auxiliar[lastFilled] = nums1[i];
-            }
-            return FindMedianSortedArrays(nums1, nums2, median1pos, i12, i21, i22, auxiliar, lastFilled);
-        }
-        else{
-            for(int i=i21; i<median2pos;i++){
-                lastFilled++;
-                auxiliar[lastFilled] = nums2[i];
-            }
-            return FindMedianSortedArrays(nums1,nums2,i11,i12,median2pos,i22,auxiliar,lastFilled);
         }
     }
 }
