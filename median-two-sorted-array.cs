4e76b064fa7a1f4cd806d09de0e02bb477e49e8d
Masiel Villalba <villalbamasiel@gmail.com>
2022-01-01 16:20:17 -0500

    fixing all...

--- median-two-sorted-array.cs
+++ median-two-sorted-array.cs
@@ -5,47 +5,77 @@ public class Program
 {
 	public static void Main()
 	{
-        // 1 1 1 2 2 2 3 6 7 
-        /*
-        int[] nums1 = new int[4]{1,1,2,3};
-        int[] nums2 = new int[5]{1,2,2,6,7};        
-
+        /*      
         int[] nums1 = new int[3]{1,2,3};
         int[] nums2 = new int[4]{4,5,6,7};
+        ok!!  ~ Cuando compara el 3 con el 4 este ultimo queda (3 y 3) y median(4) da true
 
         int[] nums1 = new int[1]{2};
         int[] nums2 = new int[2]{1,3};
-        
-        int[] nums1 = new int[4]{1,3,5,7};
-        int[] nums2 = new int[4]{2,3,6,8};        
+        ok!!  ~ Se queda vacio un array y el otro con un solo candidato   
 
         int[] nums1 = new int[4]{1,3,5,7};
         int[] nums2 = new int[5]{2,4,6,8,10};
-        
+        ok!!  ~ Se queda vacio un array y el otro con un solo candidato
+
+        int[] nums1 = new int[8]{7,8,8,8,8,8,8,9};
+        int[] nums2 = new int[26]{1,1,1,1,1,1,1,2,3,3,3,6,7,8,8,8,9,9,9,9,9,9,9,9,9,9};
+        ok!!  ~ Quedan solo 2 candidatos al final
 
         Casos problematicos:
-            Los arrays son iguales o con diferencia 2.
-
-
-        int[] nums1 = new int[4]{7,10,21,44};
-        int[] nums2 = new int[4]{22,30,31,40};
-        // Este caso es super problematico porque deja el array de la izquierda vacio
-
-
-        int[] nums1 = new int[7]{7,10,21,44,45,46,47};
-        int[] nums2 = new int[7]{22,30,31,40,40,40,40};
-        // Otro caso problematico xq quedan dos numeros al final q no se descartan y es impo-
-        // sible que puedan ser mediana juntos xq no quedan contiguos en la ordenacion
+            Los arrays son iguales o con diferencia par.
 
         int[] nums1 = new int[3]{1,20,30};
         int[] nums2 = new int[3]{1,21,32};
-        // Caso super problematico: quedan 3 numeros q no pueden descartarse y se enfrentan
-        // las medianas desde el principio
+        // Tres numeros q no se pueden descartar
 
+        int[] nums1 = new int[4]{1,1,2,3};
+        int[] nums2 = new int[5]{1,2,2,6,7};  
+        // Tres numeros q no se pueden descartar
+
+        int[] nums1 = new int[4]{1,3,5,7};
+        int[] nums2 = new int[4]{2,3,6,8};  
+        // Tres numeros q no se pueden descartar
+
+        int[] nums1 = new int[7]{7,10,21,44,45,46,47};
+        int[] nums2 = new int[7]{22,30,31,40,40,40,40};
+        // Tres numeros q no se pueden descartar
+
+        int[] nums1 = new int[4]{7,10,21,44};
+        int[] nums2 = new int[4]{22,30,31,40};
+        // Tres numeros q no se pueden descartar
+
+        int[] nums1 = new int[4]{1,2,3,4};
+        int[] nums2 = new int[8]{1,2,3,4,5,6,7,8};
+        // Cuatro numeros que no se pueden descartar
+
+        int[] nums1 = new int[4]{1,2,3,4};
+        int[] nums2 = new int[10]{1,2,3,4,5,6,7,8,8,8};
+        // Tres numeros q no se pueden descartar
+
+        int[] nums1 = new int[4]{1,2,3,3};
+        int[] nums2 = new int[20]{1,2,3,3,3,6,7,8,8,8,9,9,9,9,9,9,9,9,9,9};
+        // El primero se acaba y da IndexOutRange xq en el otro quedan mas de dos elementos
+
+        int[] nums1 = new int[4]{7,8,8,9};
+        int[] nums2 = new int[20]{1,2,3,3,3,6,7,8,8,8,9,9,9,9,9,9,9,9,9,9};
+        // Cinco elementos que no se pueden descartar (pero se reduce a 4 que no se pueden descartar)
+
+        int[] nums1 = new int[3]{8,8,9};
+        int[] nums2 = new int[2]{8,9};
+        // Cuatro numeros que no se pueden descartar
+
+        int[] nums1 = new int[10]{1,1,1,1,1,1,1,1,1,1};
+        int[] nums2 = new int[10]{1,1,1,1,1,1,1,1,1,1};
+        // Cuatro numeros que no se pueden descartar
+
+        int[] nums1 = new int[10]{0,0,0,0,0,1,1,1,1,1};
+        int[] nums2 = new int[10]{1,1,1,1,1,1,1,1,1,1};
+        // Tres numeros que no se pueden descartar
         */
 		Solution sol = new Solution();
-        int[] nums1 = new int[4]{1,3,5,7};
-        int[] nums2 = new int[4]{2,3,6,8};
+        int[] nums1 = new int[10]{0,0,0,0,0,1,1,1,1,1};
+        int[] nums2 = new int[10]{1,1,1,1,1,1,1,1,1,1};
 
         double answer = sol.FindMedianSortedArrays(nums1,nums2);
         Console.WriteLine(answer);
@@ -82,13 +112,14 @@ public class Solution {
             Console.WriteLine();
             //Console.ReadLine();
 
-            if(i11>i12){
-                if(totalLen%2==0 && i22-i21==1) return (nums2[i21]+nums2[22])/2;
-                else if(totalLen%2!=0 && i21==i22) return nums2[21];
+            if(i11==i12 && i21==i22 && totalLen%2==0) return (nums1[i11]+nums2[i22])/2;
+            else if(i11>i12){
+                if(totalLen%2==0) return (nums2[posmedian2]+nums2[posmedian2+1])/2;
+                else return nums2[posmedian2];
             }
             else if(i21>i22){
-                if(totalLen%2==0 && i12-i11==1) return (nums1[i11]+nums1[i12])/2;
-                else if(totalLen%2!=0 && i11==i12) return nums1[i11];
+                if(totalLen%2==0) return (nums1[posmedian1]+nums1[posmedian1+1])/2;
+                else return nums1[posmedian1];
             }
             
             options[0,0] = nums1[posmedian1];
@@ -121,7 +152,7 @@ public class Solution {
 
             if(totalLen%2!=0 && cond1) return options[0,0];
             else if(totalLen%2!=0 && cond2) return options[1,0];
-            //else if(totalLen%2==0 && cond1 && cond2) return (options[0,0] + options[1,0])/2;
+            else if(totalLen%2==0 && cond1 && cond2) return (options[0,0] + options[1,0])/2;
 
             int dep1 = isDeprecated(options[0,1], options[0,2]);
             int dep2 = isDeprecated(options[1,1], options[1,2]);
@@ -131,12 +162,6 @@ public class Solution {
 
             if(options[1,1]>=umbral) i22=posmedian2+dep2;
             else if(options[1,2]>=umbral) i21=posmedian2+dep2;
-
-            if(dep1==0 && dep2==0){
-                // I'm not very sure
-                if(i11==i12 && i21==i22 && totalLen%2==0) return (nums1[i11]+nums2[i22])/2;
-            }
-            //else if(totalLen%2==0) return (options[0,0] + options[1,0])/2;
         }
         return Math.Pow(-10, 7);
     }
