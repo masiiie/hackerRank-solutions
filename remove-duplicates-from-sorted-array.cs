6ada714d07ab163cce1836a9372cc0505345994a
masiiie <villalbamasiel@gmail.com>
2022-01-04 21:44:27 +0100

    remove-duplicates-from-sorted-array wrong answer

--- remove-duplicates-from-sorted-array.cs
+++ remove-duplicates-from-sorted-array.cs
@@ -1,39 +1,19 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-using System;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Solution sol = new Solution();
-        //int[] nums = new int[10]{0,0,1,1,1,2,2,3,3,4};
-        int[] nums = new int[3]{1,1,2};
-        Console.WriteLine(sol.RemoveDuplicates(nums));
-        for(int i =0;i<nums.Length;i++){
-            Console.Write(nums[i]);
-        }
-    }
-}
-
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        int k = 0;
-        int maxValue = nums[0];
-        int lastCompareIndex = 0;
+        int total = 0;
         
-        for(int i=1;i<nums.Length;i++){
-            if(nums[i]<=maxValue){
-                nums[i]=-1;
-                k++;
-            }
-            else{
-                maxValue = nums[i];
-                lastCompareIndex++; 
-                nums[lastCompareIndex] = nums[i];
-                nums[i] = -1;
+        for(int i=0;i<nums.Length;i++){
+            if(nums[i]==-1) nums[i] = i+total<nums.Length ? nums[i+total] : -1;
+            for(int j=i+Math.Max(1,total);j<nums.Length;j++){
+                if(nums[i]==nums[j]){
+                    nums[j]=-1;
+                    total++;
+                }
+                else break;
             }
         }
-        return k;
+        return nums.Length-total;
     }
 }
