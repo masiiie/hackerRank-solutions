add98cabc4642466b1386a94d754829acc12474e
masiiie <villalbamasiel@gmail.com>
2022-01-04 20:18:41 +0100

    interview-1 done! Runtime: 124 ms Memory Usage: 40.6 MB

---
+++ interview-1.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int[] RelativeSortArray(int[] arr1, int[] arr2) {
+        int index = 0;
+        int lastI = -1;
+        for(int i=0;i<arr1.Length && index<arr2.Length;i++){
+            if(arr1[i]==arr2[index]) continue;
+            else{
+                for(int j=i+1;j<arr1.Length;j++){
+                    if(arr1[j]==arr2[index]){
+                        int temp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = temp;
+                        while(arr1[i]==arr2[index]) i++;
+                    }
+                }            
+                if(arr1[i]!=arr2[index]) i--;
+                index++;
+            }
+            lastI=i;
+        }
+
+        if(lastI<arr1.Length-1 && index>=arr2.Length){
+            for(int i=lastI+1;i<arr1.Length;i++){
+                for(int j=i+1;j<arr1.Length;j++){
+                    if(arr1[j]<arr1[i]){
+                        int temp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = temp;
+                    }
+                }
+            }
+        }
+
+        return arr1;
+    }
+}
