40f7fc3cb7d430d25c43cc1ecec3fdec191544f2
masiiie <villalbamasiel@gmail.com>
2022-01-07 20:18:56 +0100

    Runtime: 260 ms, faster than 12.41% of C# online submissions for Two Sum II - Input Array Is Sorted. Memory Usage: 41.6 MB, less than 61.14% of C# online submissions for Two Sum II - Input Array Is Sorted.

--- two-sum-ii-input-array-is-sorted.cs
+++ two-sum-ii-input-array-is-sorted.cs
@@ -1,5 +1,13 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        
+    public int[] TwoSum(int[] numbers, int target) {
+        int medium = numbers.Length/2;
+        int final = target<=numbers[medium] ? medium+1 : numbers.Length;
+
+        for(int i=0;i<final;i++){
+            for(int j=i+1;j<final;j++){
+                if(numbers[i]+numbers[j]==target) return new int[2]{i+1,j+1};
+            }
+        }
+        return new int[2]{0,1};
     }
 }
