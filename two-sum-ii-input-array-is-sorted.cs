da5c27f14cf499eab79ab4a329f0a1e00b198559
masiiie <villalbamasiel@gmail.com>
2022-01-07 20:44:12 +0100

    TwoSum O(n) solution

--- two-sum-ii-input-array-is-sorted.cs
+++ two-sum-ii-input-array-is-sorted.cs
@@ -11,3 +11,21 @@ class Solution {
         return new int[2]{0,1};
     }
 }
+
+class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int medium = numbers.Length/2;
+        int final = target<=numbers[medium] ? medium+1 : numbers.Length;
+
+        int i=0;
+        int j=numbers.Length-1;
+
+        while(i<j){
+            int suma = numbers[i]+numbers[j];
+            if(suma==target) break;
+            else if(suma>target) j--;
+            else i++;
+        }
+        return new int[2]{i+1,j+1};
+    }
+}
