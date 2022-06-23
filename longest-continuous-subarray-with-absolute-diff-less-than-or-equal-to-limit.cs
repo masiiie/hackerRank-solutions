dc34905ea3a10b1d4539f0419549235fb306bbf1
Masiel Villalba <villalbamasiel@gmail.com>
2022-06-23 00:11:11 -0400

    longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit wrong answer

--- longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cs
+++ longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cs
@@ -1,27 +1,46 @@
 class Solution {
     public int LongestSubarray(int[] nums, int limit) {
-        int max = nums[0];
-        int min = nums[0];
+        SortedList<int, int> interval = new SortedList<int, int>();
+        Dictionary<int, int> quantities = new Dictionary<int, int>();
+        int start = 0;
         int maxLen = 1;
         int currLen = 1;
 
         for (int i = 0; i < nums.Length; i++)
         {
-            max = Math.Max(max, nums[i]);
-            min = Math.Min(min, nums[i]);
+            int key = nums[i];
+            int index = interval.IndexOfKey(key);
+            if(index > -1) quantities[key]++;
+            else{
+                quantities.Add(key, 1);
+                interval.Add(key, key);
+            }
 
-            if(Math.Abs(max-min) <= limit){
+            int min = interval.First().Value;
+            int max = interval.Last().Value;
+
+            if(Math.Abs(max - min) <= limit){
                 currLen++;
                 maxLen = Math.Max(maxLen, currLen);
+            }
+            else if(nums[start]==min && quantities[min]==1){
+                quantities[min]--;
+                start++;
+                interval.RemoveAt(0);
+            }
+            else if(nums[start]==max && quantities[max]==1){
+                quantities[max]--;
+                start++;
+                interval.RemoveAt(interval.Count()-1);
             }
             else{
                 max = nums[i];
                 min = nums[i];
                 currLen = 1;
+                start = i;
             }
         }
 
-        Console.WriteLine("max: {0}, min: {1}", max, min);
         return maxLen;
     }
 }
