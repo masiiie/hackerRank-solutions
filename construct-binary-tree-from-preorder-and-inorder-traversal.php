d9a1ecf39cd5d89920ef3bd9a2ca25f19ea63649
Masiel Villalba <villalbamasiel@gmail.com>
2022-06-27 13:36:50 -0400

    https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/ wrong answer

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal.php
@@ -0,0 +1,43 @@
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $preorder
+     * @param Integer[] $inorder
+     * @return TreeNode
+     */
+    function buildTree($preorder, $inorder) {
+        $N = count($preorder);
+        $value = $preorder[0];
+        if($N == 0) return null;
+        if($N == 1) return new TreeNode($value, null, null);
+        
+        $leftCount = 0;
+        for($i = 0; $i < count($inorder); $i++){
+            if($inorder[$i] != $value) $leftCount++;
+        }
+        
+        $left1= array_slice($preorder, 1, $leftCount + 1);
+        $rigth1= array_slice($preorder, $leftCount + 1, $N);
+        
+        $left2 = array_slice($inorder, 0, $leftCount);
+        $rigth2 = array_slice($inorder, $leftCount + 1, $N);
+        
+        $left = $this->buildTree($left1, $left2);
+        $rigth = $this->buildTree($rigth1, $rigth2);
+        return new TreeNode($value, $left, $rigth); 
+    }
+}
