712945e00280f79a5ed8d13f18c8a1e34aa203ee
Masiel Villalba <villalbamasiel@gmail.com>
2022-07-16 13:47:38 -0400

    Runtime: 78 ms, faster than 97.10% of JavaScript online submissions for Max Area of Island. Memory Usage: 45.1 MB, less than 68.43% of JavaScript online submissions for Max Area of Island.

---
+++ max-area-of-island.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+ var maxAreaOfIsland = function(grid) {
+    let answer = 0; 
+    let seen = newBiArray(grid.length, grid[0].length);
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            answer = Math.max(answer, area(i, j, grid, seen));
+        }
+    }    
+    return answer;
+};
+
+var newBiArray = function(r, c){
+    var gfg = new Array(r);
+    for (var i = 0; i < gfg.length; i++) {
+        gfg[i] = new Array(c);
+    } 
+    return gfg;
+}
+
+/**
+ * @param {number} r
+ * @param {number} c
+ * @param {number[][]} grid
+ * @param {boolean[][]} seen 
+ * @return {number}
+ */
+var area = function(r, c, grid, seen){
+    if(r < 0 || c < 0 || r == grid.length || c == grid[0].length || grid[r][c] == 0 || seen[r][c]) return 0;
+
+    seen[r][c] = true;
+    return 1 + area(r, c-1, grid, seen) + area(r-1, c, grid, seen) + area(r, c+1, grid, seen) + area(r+1, c, grid, seen);
+}
