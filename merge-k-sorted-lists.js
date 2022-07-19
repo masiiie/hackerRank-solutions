8bb378c72fc5ba58c8579fd39c27eb3a737a9d2d
masiiie <villalbamasiel@gmail.com>
2022-07-19 15:37:12 -0400

    Runtime: 525 ms, faster than 24.72% of JavaScript online submissions for Merge k Sorted Lists. Memory Usage: 47.6 MB, less than 53.94% of JavaScript online submissions for Merge k Sorted Lists.

--- merge-k-sorted-lists.js
+++ merge-k-sorted-lists.js
@@ -1,46 +1,27 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
- var mergeKLists = function(lists) {
-    if(lists.length == 0) return new ListNode()
-    if(lists.length == 1) return lists[0]
-
-    let N = Math.trunc(lists.length/2) 
-    let p1 = mergeKLists(lists.slice(0, N))
-    let p2 = mergeKLists(lists.slice(N))
-
-    return merge(p1, p2)
-};
-
-
-var merge = function(l1, l2){
-    let sol = new ListNode()
-    let solHead = sol
-
-    while(l1 && l2){
-        if(l1.val < l2.val){
-            solHead.val = l1.val
-            l1 = l1.next
+var mergeKLists = function(lists) {
+    const merge = (l1, l2) => {
+      if (!l1 || !l2) return l1 || l2;
+      let node = {};
+      const root = node;
+      while (l1 && l2) {
+        if (l1.val <= l2.val) {
+          node.next = l1;
+          l1 = l1.next;
+        } else {
+          node.next = l2;
+          l2 = l2.next;
         }
-        else{
-            solHead.val = l2.val
-            l2 = l2.next
-        }
-
-        solHead.next = new ListNode()
-        solHead = solHead.next
+        node = node.next;
+      }
+      if (l1) node.next = l1;
+      if (l2) node.next = l2;
+      return root.next;
     }
     
-    if(l1) solHead = l1
-    if(l2) solHead = l2
-
-    return sol
-}
+    let root = lists[0];
+    for (let i = 1; i < lists.length; i++) {
+      root = merge(root, lists[i]);
+    }
+    
+    return root || null;
+  };
