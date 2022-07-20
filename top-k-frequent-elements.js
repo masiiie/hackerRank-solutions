c8672adeea49b8f99b4540e9ee59c99ed5725002
masiiie <villalbamasiel@gmail.com>
2022-07-20 09:50:06 -0400

    Runtime: 186 ms, faster than 10.00% of JavaScript online submissions for Top K Frequent Elements. Memory Usage: 45.2 MB, less than 63.83% of JavaScript online submissions for Top K Frequent Elements.

--- top-k-frequent-elements.js
+++ top-k-frequent-elements.js
@@ -5,20 +5,20 @@
  */
  var topKFrequent = function(nums, k) {
     let count = {}
-    let freq = new Array(nums.length).fill([]);
+    let freq = new Array(nums.length + 1).fill(null);
 
     for (let i = 0; i < nums.length; i++) {
-        const index = freq[count[nums[i]] - 1].indexOf(nums[i]);
-        if(index > -1) freq[count[nums[i]] - 1].splice(index, 1) 
-        if(count[nums[i]] == undefined) count[nums[i]] = 0
+        count[nums[i]] = count[nums[i]] == undefined ? 0 : count[nums[i]]
+        let cNum = count[nums[i]]
+        if(freq[cNum] != null) freq[cNum].delete(nums[i])
         count[nums[i]]++
-
-        freq[count[nums[i]] - 1].push(nums[i])
+        if(freq[cNum+1] == null) freq[cNum+1] = new Set()
+        freq[cNum+1].add(nums[i])
     }
 
     let sol = []
-    for (let i = freq.length-1; i > -1; i++) {
-        if(freq[i].length > 0) sol = sol.concat(freq[i])  
+    for (let i = freq.length-1; i > -1 && sol.length < k; i--) {
+        if(freq[i] != null && freq[i].size > 0) sol = sol.concat(Array.from(freq[i]).slice(0, k-sol.length))  
     }
     return sol
 };
