f39d37f97079bb2ae78f3c05b1d5d3450a2b841e
masiiie <villalbamasiel@gmail.com>
2022-07-20 11:33:24 -0400

    Runtime: 111 ms, faster than 54.69% of JavaScript online submissions for Sort Array by Increasing Frequency. Memory Usage: 44.9 MB, less than 36.25% of JavaScript online submissions for Sort Array by Increasing Frequency.

--- sort-array-by-increasing-frequency.js
+++ sort-array-by-increasing-frequency.js
@@ -1,24 +1,18 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
- var frequencySort = function(nums) {
-    let count = {}
-    let freq = new Array(nums.length + 1).fill(null);
-
+var frequencySort = function(nums) {
+    let maps = new Map()
+    
     for (let i = 0; i < nums.length; i++) {
-        count[nums[i]] = count[nums[i]] == undefined ? 0 : count[nums[i]]
-        let cNum = count[nums[i]]
-        if(freq[cNum] != null) freq[cNum].delete(nums[i])
-        count[nums[i]]++
-        if(freq[cNum+1] == null) freq[cNum+1] = new Set()
-        freq[cNum+1].add(nums[i])
+        maps.has(nums[i]) ? maps.set(nums[i],maps.get(nums[i]) + 1) : maps.set(nums[i], 1)
     }
-
-    let sol = []
-    for (let i = 1; i < freq.length; i++) {
-        if(freq[i] == null) continue
-        for (const itm of freq[i]) sol = sol.concat(new Array(i).fill(itm))  
-    }
-    return sol    
+    
+    let sorted = [...maps.entries()].sort((a,b) => a[1]===b[1] ? b[0] - a[0] : a[1] - b[1])
+    
+    let ret = []
+    sorted.forEach(x=> {
+        while(x[1] > 0) {
+            ret.push(x[0])
+            x[1]--
+        }
+    })
+    return ret
 };
