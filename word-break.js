52080c6d698bbaec66ce1a32a18032e55fd9f394
Masiel Villalba <villalbamasiel@gmail.com>
2022-07-20 22:27:27 -0400

    Runtime: 82 ms, faster than 77.19% of JavaScript online submissions for Word Break. Memory Usage: 43.1 MB, less than 88.22% of JavaScript online submissions for Word Break.

--- word-break.js
+++ word-break.js
@@ -1,31 +1,22 @@
-class TreeNode {
-    constructor(value = key, parent = null) {
-        this.value = value;
-        this.parent = parent;
-    }
-}
-
-
-/**
+/**   
  * @param {string} s
  * @param {string[]} wordDict
  * @return {boolean}
  */
  var wordBreak = function(s, wordDict) {
     let falsePaths = new Set()
-    let path = new TreeNode(-1, null)
+    let path = []
     
     var aux = function(idx){
         if(idx == s.length) return true
 
         for (let i = 0; i < wordDict.length; i++) {
             let word = wordDict[i]
-            let newState = 
-            if(s.slice(idx, idx + word.length) == word && !falsePaths.has()){
+            if(s.slice(idx, idx + word.length) == word && !falsePaths.has(idx + word.length)){
                 path.push(i)
                 let ans = aux(idx + word.length)
                 if(ans) return ans
-                falsePaths.add((idx,Array.from(path)))
+                falsePaths.add(idx + word.length)
                 path.pop()
             }
         }
