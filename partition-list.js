45d97536ae9fec2ac0f1713d630ae588ef76e1bc
Masiel Villalba <villalbamasiel@gmail.com>
2022-07-22 11:52:58 -0400

    Runtime: 96 ms, faster than 49.85% of JavaScript online submissions for Partition List. Memory Usage: 43.8 MB, less than 52.17% of JavaScript online submissions for Partition List.

--- partition-list.js
+++ partition-list.js
@@ -1,36 +1,18 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} x
- * @return {ListNode}
- */
- var partition = function(head, x) {
-    let greaterThanHead = null
-    let greaterThan = new ListNode()
-    let prev = null
-    let lessThan = head
-
-    while (lessThan) {
-        if(lessThan.val >= x){
-            greaterThan.val = lessThan.val
-            greaterThan.next = new ListNode(0, null)
-            if(greaterThanHead == null) greaterThanHead = greaterThan
-            greaterThan = greaterThan.next
-
-            if(prev != null) prev.next = prev.next.next 
+var partition = function(head, x) {
+    const dummyLess = new ListNode();
+    const dummyMore = new ListNode();
+    let node = head;
+    let less = dummyLess;
+    let more = dummyMore;
+    while (node) {
+        if (node.val < x) {
+            less = less.next = node;
+        } else {
+            more = more.next = node;
         }
-        else if(lessThan.val < x){
-            if(prev == null) head = lessThan
-            prev = lessThan
-        }
-        lessThan = lessThan.next
+        node = node.next;
     }
-    if(prev != null) prev.next = greaterThanHead
-    return head
+    less.next = dummyMore.next;
+    more.next = null;
+    return dummyLess.next;
 };
