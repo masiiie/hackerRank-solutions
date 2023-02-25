b9f240565244b57d2768ba64341daeac614ff776
Masiel Villalba <villalbamasiel@gmail.com>
2023-02-25 02:27:38 -0500

    Runtime 83 ms Beats 85.55% Memory 51.6 MB Beats 63.32%

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    let min = Number.MAX_VALUE;
     let mxPr = 0;
     for (let i = 0; i < prices.length; i++) {
-        for (let j = i+1; j < prices.length; j++) {
-            let dif = prices[j] - prices[i];
-            if(dif > mxPr) mxPr = dif;
-        }  
+        if(prices[i] < min) min = prices[i];
+        if(prices[i] - min > mxPr) mxPr = prices[i] - min;
     }
     return mxPr;
 };
